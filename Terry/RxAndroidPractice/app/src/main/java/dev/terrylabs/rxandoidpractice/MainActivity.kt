98d57b85633c0e7962d0699318e887acbdcccaa2
della-padula <della.kimko@gmail.com>
2019-08-24 23:21:52 +0900

    Add All FIles

--- Terry/RxAndroidPractice/app/src/main/java/dev/terrylabs/rxandoidpractice/MainActivity.kt
+++ Terry/RxAndroidPractice/app/src/main/java/dev/terrylabs/rxandoidpractice/MainActivity.kt
@@ -8,8 +8,6 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val OWNER = "della-padula"
-
 class MainActivity : AppCompatActivity() {
 
     private var recyclerViewAdapter: RecyclerViewAdapter = RecyclerViewAdapter()
